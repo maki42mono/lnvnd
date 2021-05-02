2dee95c8223e08b672b6dcb224d3e95e3507495c
maki_mono <mapooff@gmail.com>
2021-05-02 16:04:34 +0300

    Добавил загрузку файлов

---
+++ library/Command.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace library;
+
+class Command
+{
+    public static function test(): void
+    {
+        echo "Hello world!";
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+$library = function ($classname) {
+    $path = __DIR__ . "/../{$classname}.php";
+    $path = str_replace("/", DIRECTORY_SEPARATOR, $path);
+    if (file_exists($path)) {
+        require_once ($path);
+    }
+};
+
+\spl_autoload_register($library);
+
+\library\Command::test();
