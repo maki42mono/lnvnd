7d0de23d7bc610d435b670657c1a2c08fc801cc8
Maksim Pukh <packagist@makimono.ru>
2021-05-03 22:16:50 +0300

    Добавил проверку на запуск из браузера

--- public/index.php
+++ public/index.php
@@ -12,18 +12,28 @@ $library = function ($classname) {
 
 try {
     \library\Conf::instance();
-    //todo: добавить проверку на то, что запуск именно из консоли
-//    \library\Parser::readCommand();
+//    Если запрос из браузера — то найти существующую команду, поменять у нее имя, аргументы и добавить опции
+    if (isset($_SERVER['HTTP_USER_AGENT'])) {
+        $command_name = "command_name_12";
+        if (library\Command::hasOne(['name' => $command_name])) {
+            $command = \library\Command::findOne(['name' => $command_name]);
+            $time = time();
+            $command->setName("modified_name{$time}");
+            $command->setArguments(["modified_arguments"]);
+            $command->addOption(new \library\Option("new_option", [
+                "new_param_1",
+                "new_param_2",
+            ]));
+            $command->save();
+            echo $command;
+        } else {
+            echo "Команда {$command_name} не зарегистрирована. Попробуйте зарегистрировать через консоль =)";
+        }
 
-    $command = \library\Command::findOne(['name' => "command_name_12"]);
-    $command->setName("modified_name");
-    $command->setArguments(["modified_arguments"]);
-    $command->addOption(new \library\Option("new_option", [
-        "new_param_1",
-        "new_param_2",
-    ]));
-    $command->save();
-    echo $command;
+    } else {
+//        Иначе получить команду из консоли
+        \library\Parser::readCommand();
+    }
 } catch (\Exception $e) {
     echo $e->getMessage();
 }
