a209c73148828c04cdc4fda1bff75987f1d55949
Maksim Pukh <packagist@makimono.ru>
2021-05-06 17:06:42 +0300

    Поправил пути подключения

--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -1,6 +1,7 @@
 <?php declare(strict_types=1);
-
-require_once("../vendor/autoload.php");
+$path = __DIR__ . "/../vendor/autoload.php";
+$path = str_replace('\\', DIRECTORY_SEPARATOR, $path);
+require_once($path);
 
 use PHPUnit\Framework\TestCase;
 
